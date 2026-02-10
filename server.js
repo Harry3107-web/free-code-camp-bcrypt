48e6fda779faa64d39e0fa6ecd635d6aaf9418dc
Karthik Ramesh <kathikramesh317@gmail.com>
2026-02-10 23:56:00 +0530

    Add hash and hash compare functions

--- server.js
+++ server.js
@@ -9,8 +9,17 @@ const saltRounds = 12;
 const myPlaintextPassword = 'sUperpassw0rd!';
 const someOtherPlaintextPassword = 'pass123';
 
-
+// Define a GET route for the root URL
+app.get('/', (req, res) => {
+  res.send('Hello World!'); // Sends a text response back to the client
+});
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
+bcrypt.hash('MyplaintextPassword', saltRounds, (err, hash) => {
+    console.log(hash)
+   bcrypt.compare('MyplaintextPassword', hash, (err, res) => {
+    console.log(res)
+  });
+});
 
 
 
